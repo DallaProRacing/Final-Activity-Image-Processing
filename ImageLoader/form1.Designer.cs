d3252bc1215132357aac8e779c168e0f14f72d02
DallaProRacing <103423@aluno.uricer.edu.br>
2025-03-11 12:33:34 -0300

    Implemented addition and subtraction operations

---
+++ ImageLoader/form1.Designer.cs
@@ -0,0 +1,252 @@
+﻿namespace ImageLoader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btImg1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnSum = new System.Windows.Forms.Button();
+            this.btnSubt = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtSum = new System.Windows.Forms.TextBox();
+            this.txtSubt = new System.Windows.Forms.TextBox();
+            this.btnReset = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btImg1
+            // 
+            this.btImg1.BackColor = System.Drawing.Color.White;
+            this.btImg1.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btImg1.ForeColor = System.Drawing.Color.Black;
+            this.btImg1.Location = new System.Drawing.Point(64, 96);
+            this.btImg1.Name = "btImg1";
+            this.btImg1.Size = new System.Drawing.Size(125, 24);
+            this.btImg1.TabIndex = 0;
+            this.btImg1.Text = "Upload";
+            this.btImg1.UseVisualStyleBackColor = false;
+            this.btImg1.Click += new System.EventHandler(this.btImg1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Location = new System.Drawing.Point(25, 126);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(202, 161);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.White;
+            this.pictureBox2.Location = new System.Drawing.Point(27, 474);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(202, 161);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(77, 454);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Uploaded Image";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.AllowDrop = true;
+            this.panel1.AutoScroll = true;
+            this.panel1.BackColor = System.Drawing.Color.DarkCyan;
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.pictureBox2);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.btImg1);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(257, 800);
+            this.panel1.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Algerian", 7F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(5, 45);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(107, 11);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Ruan C. Dalla Rosa";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Algerian", 20F, System.Drawing.FontStyle.Italic);
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(-1, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(258, 30);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Image Processing";
+            // 
+            // btnSum
+            // 
+            this.btnSum.BackColor = System.Drawing.Color.White;
+            this.btnSum.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSum.ForeColor = System.Drawing.Color.Black;
+            this.btnSum.Location = new System.Drawing.Point(278, 40);
+            this.btnSum.Name = "btnSum";
+            this.btnSum.Size = new System.Drawing.Size(67, 24);
+            this.btnSum.TabIndex = 7;
+            this.btnSum.Text = "Sum";
+            this.btnSum.UseVisualStyleBackColor = false;
+            this.btnSum.Click += new System.EventHandler(this.btnSum_Click);
+            // 
+            // btnSubt
+            // 
+            this.btnSubt.BackColor = System.Drawing.Color.White;
+            this.btnSubt.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSubt.ForeColor = System.Drawing.Color.Black;
+            this.btnSubt.Location = new System.Drawing.Point(278, 70);
+            this.btnSubt.Name = "btnSubt";
+            this.btnSubt.Size = new System.Drawing.Size(67, 24);
+            this.btnSubt.TabIndex = 8;
+            this.btnSubt.Text = "Subtract";
+            this.btnSubt.UseVisualStyleBackColor = false;
+            this.btnSubt.Click += new System.EventHandler(this.btnSubt_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Black;
+            this.label4.Location = new System.Drawing.Point(275, 13);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(141, 15);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Mathematical Operations";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // txtSum
+            // 
+            this.txtSum.Font = new System.Drawing.Font("Times New Roman", 9.75F);
+            this.txtSum.Location = new System.Drawing.Point(351, 40);
+            this.txtSum.Name = "txtSum";
+            this.txtSum.Size = new System.Drawing.Size(65, 22);
+            this.txtSum.TabIndex = 9;
+            // 
+            // txtSubt
+            // 
+            this.txtSubt.Font = new System.Drawing.Font("Times New Roman", 9.75F);
+            this.txtSubt.Location = new System.Drawing.Point(351, 72);
+            this.txtSubt.Name = "txtSubt";
+            this.txtSubt.Size = new System.Drawing.Size(65, 22);
+            this.txtSubt.TabIndex = 10;
+            // 
+            // btnReset
+            // 
+            this.btnReset.BackColor = System.Drawing.Color.White;
+            this.btnReset.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReset.ForeColor = System.Drawing.Color.Black;
+            this.btnReset.Location = new System.Drawing.Point(435, 12);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(67, 24);
+            this.btnReset.TabIndex = 11;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = false;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.GhostWhite;
+            this.ClientSize = new System.Drawing.Size(759, 645);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.txtSubt);
+            this.Controls.Add(this.txtSum);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnSubt);
+            this.Controls.Add(this.btnSum);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Corbel", 8.25F);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Image Processing";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btImg1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnSum;
+        private System.Windows.Forms.Button btnSubt;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtSum;
+        private System.Windows.Forms.TextBox txtSubt;
+        private System.Windows.Forms.Button btnReset;
+    }
+}
+
---
+++ ImageLoader/form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace ImageLoader
+{
+    public partial class Form1 : Form
+    {
+        Bitmap img1; // Imagem original
+        Bitmap img2; // Imagem alterada (cópia da original que será modificada)
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btImg1_Click(object sender, EventArgs e)
+        {
+            // Configurações iniciais da OpenFileDialog
+            openFileDialog1.InitialDirectory = "C:\\Matlab";
+            openFileDialog1.Filter = "TIFF image (*.tif)|*.tif|JPG image (*.jpg)|*.jpg|BMP image (*.bmp)|*.bmp|PNG image (*.png)|*.png|All files (*.*)|*.*";
+            openFileDialog1.FilterIndex = 2;
+            openFileDialog1.RestoreDirectory = true;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    img1 = new Bitmap(openFileDialog1.FileName);
+                    img2 = new Bitmap(img1); // Cria uma cópia para edições
+
+                    pictureBox1.Image = img1; // Exibe a imagem original
+                    pictureBox2.Image = new Bitmap(img1); // Exibe a cópia na PictureBox2
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Erro ao abrir imagem...", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void btnSum_Click(object sender, EventArgs e)
+        {
+            if (!int.TryParse(txtSum.Text, out int sumValue) || sumValue < 0 || sumValue > 255)
+            {
+                MessageBox.Show("Digite um valor entre 0 e 255 para aumentar o brilho.", "Entrada Inválida", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            // Aplicar o aumento de brilho
+            img2 = AjustarBrilho(img2, sumValue);
+            pictureBox2.Image = img2;
+            txtSum.Clear();
+        }
+
+        private void btnSubt_Click(object sender, EventArgs e)
+        {
+            if (!int.TryParse(txtSubt.Text, out int subValue) || subValue < 0 || subValue > 255)
+            {
+                MessageBox.Show("Digite um valor entre 0 e 255 para diminuir o brilho.", "Entrada Inválida", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            // Aplicar a redução de brilho
+            img2 = AjustarBrilho(img2, -subValue);
+            pictureBox2.Image = img2;
+            txtSubt.Clear();
+        }        
+        private Bitmap AjustarBrilho(Bitmap img, int ajuste)
+        {
+            Bitmap tempImg = new Bitmap(img.Width, img.Height);
+
+            for (int i = 0; i < img.Width; i++)
+            {
+                for (int j = 0; j < img.Height; j++)
+                {
+                    Color pixel = img.GetPixel(i, j);
+                    int r = Math.Max(0, Math.Min(255, pixel.R + ajuste));
+                    int g = Math.Max(0, Math.Min(255, pixel.G + ajuste));
+                    int b = Math.Max(0, Math.Min(255, pixel.B + ajuste));
+
+                    tempImg.SetPixel(i, j, Color.FromArgb(pixel.A, r, g, b));
+                }
+            }
+            return tempImg;
+        }
+        private void btnReset_Click_1(object sender, EventArgs e)
+        {
+            img2 = new Bitmap(img1); // Restaura a imagem original
+            pictureBox2.Image = img2;
+        }
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+       
+    }
+
+}
