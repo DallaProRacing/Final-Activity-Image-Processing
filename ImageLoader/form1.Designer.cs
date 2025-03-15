ca0bf047c2851d1d7f0d4acd58aa6dec795a078c
DallaProRacing <103423@aluno.uricer.edu.br>
2025-03-15 20:02:02 -0300

    feat: Implementation of multiplication, division and grayscale functions. In addition to summing and reducing one image with another

--- ImageLoader/form1.Designer.cs
+++ ImageLoader/form1.Designer.cs
@@ -31,9 +31,11 @@
             this.btImg1 = new System.Windows.Forms.Button();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBoxResult = new System.Windows.Forms.PictureBox();
             this.label1 = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.btImg2 = new System.Windows.Forms.Button();
             this.label3 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.btnSum = new System.Windows.Forms.Button();
@@ -42,9 +44,18 @@
             this.txtSum = new System.Windows.Forms.TextBox();
             this.txtSubt = new System.Windows.Forms.TextBox();
             this.btnReset = new System.Windows.Forms.Button();
+            this.txtDiv = new System.Windows.Forms.TextBox();
+            this.txtMult = new System.Windows.Forms.TextBox();
+            this.btndiv = new System.Windows.Forms.Button();
+            this.btnMult = new System.Windows.Forms.Button();
+            this.btnConvertGrayscale = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnReduce = new System.Windows.Forms.Button();
+            this.btnClean = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxResult)).BeginInit();
             this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
             // 
             // btImg1
@@ -52,7 +63,7 @@
             this.btImg1.BackColor = System.Drawing.Color.White;
             this.btImg1.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btImg1.ForeColor = System.Drawing.Color.Black;
-            this.btImg1.Location = new System.Drawing.Point(64, 96);
+            this.btImg1.Location = new System.Drawing.Point(63, 64);
             this.btImg1.Name = "btImg1";
             this.btImg1.Size = new System.Drawing.Size(125, 24);
             this.btImg1.TabIndex = 0;
@@ -67,22 +78,22 @@
             // pictureBox1
             // 
             this.pictureBox1.BackColor = System.Drawing.Color.White;
-            this.pictureBox1.Location = new System.Drawing.Point(25, 126);
+            this.pictureBox1.Location = new System.Drawing.Point(25, 90);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(202, 161);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
             // 
-            // pictureBox2
+            // pictureBoxResult
             // 
-            this.pictureBox2.BackColor = System.Drawing.Color.White;
-            this.pictureBox2.Location = new System.Drawing.Point(27, 474);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(202, 161);
-            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox2.TabIndex = 2;
-            this.pictureBox2.TabStop = false;
+            this.pictureBoxResult.BackColor = System.Drawing.Color.White;
+            this.pictureBoxResult.Location = new System.Drawing.Point(27, 481);
+            this.pictureBoxResult.Name = "pictureBoxResult";
+            this.pictureBoxResult.Size = new System.Drawing.Size(202, 161);
+            this.pictureBoxResult.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxResult.TabIndex = 2;
+            this.pictureBoxResult.TabStop = false;
             // 
             // label1
             // 
@@ -90,20 +101,21 @@
             this.label1.BackColor = System.Drawing.Color.White;
             this.label1.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.Black;
-            this.label1.Location = new System.Drawing.Point(77, 454);
+            this.label1.Location = new System.Drawing.Point(77, 461);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(94, 15);
             this.label1.TabIndex = 3;
             this.label1.Text = "Uploaded Image";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // panel1
             // 
             this.panel1.AllowDrop = true;
             this.panel1.AutoScroll = true;
             this.panel1.BackColor = System.Drawing.Color.DarkCyan;
-            this.panel1.Controls.Add(this.label3);
             this.panel1.Controls.Add(this.pictureBox2);
+            this.panel1.Controls.Add(this.btImg2);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.pictureBoxResult);
             this.panel1.Controls.Add(this.label2);
             this.panel1.Controls.Add(this.label1);
             this.panel1.Controls.Add(this.btImg1);
@@ -112,6 +124,29 @@
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(257, 800);
             this.panel1.TabIndex = 4;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.White;
+            this.pictureBox2.Location = new System.Drawing.Point(27, 285);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(202, 161);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 8;
+            this.pictureBox2.TabStop = false;
+            // 
+            // btImg2
+            // 
+            this.btImg2.BackColor = System.Drawing.Color.White;
+            this.btImg2.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btImg2.ForeColor = System.Drawing.Color.Black;
+            this.btImg2.Location = new System.Drawing.Point(63, 257);
+            this.btImg2.Name = "btImg2";
+            this.btImg2.Size = new System.Drawing.Size(125, 24);
+            this.btImg2.TabIndex = 7;
+            this.btImg2.Text = "Upload";
+            this.btImg2.UseVisualStyleBackColor = false;
+            this.btImg2.Click += new System.EventHandler(this.btImg2_Click);
             // 
             // label3
             // 
@@ -140,9 +175,9 @@
             this.btnSum.BackColor = System.Drawing.Color.White;
             this.btnSum.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnSum.ForeColor = System.Drawing.Color.Black;
-            this.btnSum.Location = new System.Drawing.Point(278, 40);
+            this.btnSum.Location = new System.Drawing.Point(278, 74);
             this.btnSum.Name = "btnSum";
-            this.btnSum.Size = new System.Drawing.Size(67, 24);
+            this.btnSum.Size = new System.Drawing.Size(91, 24);
             this.btnSum.TabIndex = 7;
             this.btnSum.Text = "Sum";
             this.btnSum.UseVisualStyleBackColor = false;
@@ -153,9 +188,9 @@
             this.btnSubt.BackColor = System.Drawing.Color.White;
             this.btnSubt.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnSubt.ForeColor = System.Drawing.Color.Black;
-            this.btnSubt.Location = new System.Drawing.Point(278, 70);
+            this.btnSubt.Location = new System.Drawing.Point(278, 104);
             this.btnSubt.Name = "btnSubt";
-            this.btnSubt.Size = new System.Drawing.Size(67, 24);
+            this.btnSubt.Size = new System.Drawing.Size(91, 24);
             this.btnSubt.TabIndex = 8;
             this.btnSubt.Text = "Subtract";
             this.btnSubt.UseVisualStyleBackColor = false;
@@ -172,12 +207,11 @@
             this.label4.Size = new System.Drawing.Size(141, 15);
             this.label4.TabIndex = 7;
             this.label4.Text = "Mathematical Operations";
-            this.label4.Click += new System.EventHandler(this.label4_Click);
             // 
             // txtSum
             // 
             this.txtSum.Font = new System.Drawing.Font("Times New Roman", 9.75F);
-            this.txtSum.Location = new System.Drawing.Point(351, 40);
+            this.txtSum.Location = new System.Drawing.Point(375, 74);
             this.txtSum.Name = "txtSum";
             this.txtSum.Size = new System.Drawing.Size(65, 22);
             this.txtSum.TabIndex = 9;
@@ -185,7 +219,7 @@
             // txtSubt
             // 
             this.txtSubt.Font = new System.Drawing.Font("Times New Roman", 9.75F);
-            this.txtSubt.Location = new System.Drawing.Point(351, 72);
+            this.txtSubt.Location = new System.Drawing.Point(375, 104);
             this.txtSubt.Name = "txtSubt";
             this.txtSubt.Size = new System.Drawing.Size(65, 22);
             this.txtSubt.TabIndex = 10;
@@ -195,7 +229,7 @@
             this.btnReset.BackColor = System.Drawing.Color.White;
             this.btnReset.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnReset.ForeColor = System.Drawing.Color.Black;
-            this.btnReset.Location = new System.Drawing.Point(435, 12);
+            this.btnReset.Location = new System.Drawing.Point(280, 36);
             this.btnReset.Name = "btnReset";
             this.btnReset.Size = new System.Drawing.Size(67, 24);
             this.btnReset.TabIndex = 11;
@@ -203,12 +237,114 @@
             this.btnReset.UseVisualStyleBackColor = false;
             this.btnReset.Click += new System.EventHandler(this.btnReset_Click_1);
             // 
+            // txtDiv
+            // 
+            this.txtDiv.Font = new System.Drawing.Font("Times New Roman", 9.75F);
+            this.txtDiv.Location = new System.Drawing.Point(375, 162);
+            this.txtDiv.Name = "txtDiv";
+            this.txtDiv.Size = new System.Drawing.Size(65, 22);
+            this.txtDiv.TabIndex = 15;
+            // 
+            // txtMult
+            // 
+            this.txtMult.Font = new System.Drawing.Font("Times New Roman", 9.75F);
+            this.txtMult.Location = new System.Drawing.Point(375, 132);
+            this.txtMult.Name = "txtMult";
+            this.txtMult.Size = new System.Drawing.Size(65, 22);
+            this.txtMult.TabIndex = 14;
+            // 
+            // btndiv
+            // 
+            this.btndiv.BackColor = System.Drawing.Color.White;
+            this.btndiv.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btndiv.ForeColor = System.Drawing.Color.Black;
+            this.btndiv.Location = new System.Drawing.Point(278, 162);
+            this.btndiv.Name = "btndiv";
+            this.btndiv.Size = new System.Drawing.Size(91, 24);
+            this.btndiv.TabIndex = 13;
+            this.btndiv.Text = "Division";
+            this.btndiv.UseVisualStyleBackColor = false;
+            this.btndiv.Click += new System.EventHandler(this.btndiv_Click);
+            // 
+            // btnMult
+            // 
+            this.btnMult.BackColor = System.Drawing.Color.White;
+            this.btnMult.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMult.ForeColor = System.Drawing.Color.Black;
+            this.btnMult.Location = new System.Drawing.Point(278, 132);
+            this.btnMult.Name = "btnMult";
+            this.btnMult.Size = new System.Drawing.Size(91, 24);
+            this.btnMult.TabIndex = 12;
+            this.btnMult.Text = "Multiplication";
+            this.btnMult.UseVisualStyleBackColor = false;
+            this.btnMult.Click += new System.EventHandler(this.btnMult_Click);
+            // 
+            // btnConvertGrayscale
+            // 
+            this.btnConvertGrayscale.BackColor = System.Drawing.Color.White;
+            this.btnConvertGrayscale.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConvertGrayscale.ForeColor = System.Drawing.Color.Black;
+            this.btnConvertGrayscale.Location = new System.Drawing.Point(278, 192);
+            this.btnConvertGrayscale.Name = "btnConvertGrayscale";
+            this.btnConvertGrayscale.Size = new System.Drawing.Size(162, 24);
+            this.btnConvertGrayscale.TabIndex = 17;
+            this.btnConvertGrayscale.Text = "Convert Grayscale";
+            this.btnConvertGrayscale.UseVisualStyleBackColor = false;
+            this.btnConvertGrayscale.Click += new System.EventHandler(this.btnConvertGrayscale_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.BackColor = System.Drawing.Color.White;
+            this.btnAdd.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdd.ForeColor = System.Drawing.Color.Black;
+            this.btnAdd.Location = new System.Drawing.Point(280, 227);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(162, 24);
+            this.btnAdd.TabIndex = 18;
+            this.btnAdd.Text = "Add the two images";
+            this.btnAdd.UseVisualStyleBackColor = false;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnReduce
+            // 
+            this.btnReduce.BackColor = System.Drawing.Color.White;
+            this.btnReduce.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReduce.ForeColor = System.Drawing.Color.Black;
+            this.btnReduce.Location = new System.Drawing.Point(280, 257);
+            this.btnReduce.Name = "btnReduce";
+            this.btnReduce.Size = new System.Drawing.Size(162, 24);
+            this.btnReduce.TabIndex = 19;
+            this.btnReduce.Text = "Reduce both images";
+            this.btnReduce.UseVisualStyleBackColor = false;
+            this.btnReduce.Click += new System.EventHandler(this.btnReduce_Click);
+            // 
+            // btnClean
+            // 
+            this.btnClean.BackColor = System.Drawing.Color.White;
+            this.btnClean.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClean.ForeColor = System.Drawing.Color.Black;
+            this.btnClean.Location = new System.Drawing.Point(375, 36);
+            this.btnClean.Name = "btnClean";
+            this.btnClean.Size = new System.Drawing.Size(67, 24);
+            this.btnClean.TabIndex = 20;
+            this.btnClean.Text = "To Clean";
+            this.btnClean.UseVisualStyleBackColor = false;
+            this.btnClean.Click += new System.EventHandler(this.btnClean_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.GhostWhite;
             this.ClientSize = new System.Drawing.Size(759, 645);
+            this.Controls.Add(this.btnClean);
+            this.Controls.Add(this.btnReduce);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.btnConvertGrayscale);
+            this.Controls.Add(this.txtDiv);
+            this.Controls.Add(this.txtMult);
+            this.Controls.Add(this.btndiv);
+            this.Controls.Add(this.btnMult);
             this.Controls.Add(this.btnReset);
             this.Controls.Add(this.txtSubt);
             this.Controls.Add(this.txtSum);
@@ -221,11 +357,11 @@
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Image Processing";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
-            this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxResult)).EndInit();
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -236,7 +372,7 @@
         private System.Windows.Forms.Button btImg1;
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
         private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBoxResult;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Label label2;
@@ -247,6 +383,16 @@
         private System.Windows.Forms.TextBox txtSum;
         private System.Windows.Forms.TextBox txtSubt;
         private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button btImg2;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.TextBox txtDiv;
+        private System.Windows.Forms.TextBox txtMult;
+        private System.Windows.Forms.Button btndiv;
+        private System.Windows.Forms.Button btnMult;
+        private System.Windows.Forms.Button btnConvertGrayscale;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnReduce;
+        private System.Windows.Forms.Button btnClean;
     }
 }
 
--- ImageLoader/form1.cs
+++ ImageLoader/form1.cs
@@ -6,8 +6,8 @@ namespace ImageLoader
 {
     public partial class Form1 : Form
     {
-        Bitmap img1; // Imagem original
-        Bitmap img2; // Imagem alterada (cópia da original que será modificada)
+        Bitmap img1, img1Original;
+        Bitmap img2;
 
         public Form1()
         {
@@ -16,27 +16,45 @@ namespace ImageLoader
 
         private void btImg1_Click(object sender, EventArgs e)
         {
-            // Configurações iniciais da OpenFileDialog
-            openFileDialog1.InitialDirectory = "C:\\Matlab";
-            openFileDialog1.Filter = "TIFF image (*.tif)|*.tif|JPG image (*.jpg)|*.jpg|BMP image (*.bmp)|*.bmp|PNG image (*.png)|*.png|All files (*.*)|*.*";
-            openFileDialog1.FilterIndex = 2;
-            openFileDialog1.RestoreDirectory = true;
+            img1 = CarregarImagem();
+            if (img1 != null)
+            {
+                img1Original = new Bitmap(img1); // Criar cópia da imagem original
+                pictureBox1.Image = img1;
+            }
+        }
 
+        private void btImg2_Click(object sender, EventArgs e)
+        {
+            img2 = CarregarImagem();
+            if (img2 != null)
+            {
+                if (img1 != null && (img1.Width != img2.Width || img1.Height != img2.Height))
+                {
+                    MessageBox.Show("As imagens devem ter o mesmo tamanho. Escolha outra imagem.", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    img2 = null;
+                    pictureBox2.Image = null;
+                    return;
+                }
+                pictureBox2.Image = img2;
+            }
+        }
+
+        private Bitmap CarregarImagem()
+        {
+            openFileDialog1.Filter = "Imagens|*.tif;*.jpg;*.bmp;*.png|Todos os arquivos|*.*";
             if (openFileDialog1.ShowDialog() == DialogResult.OK)
             {
                 try
                 {
-                    img1 = new Bitmap(openFileDialog1.FileName);
-                    img2 = new Bitmap(img1); // Cria uma cópia para edições
-
-                    pictureBox1.Image = img1; // Exibe a imagem original
-                    pictureBox2.Image = new Bitmap(img1); // Exibe a cópia na PictureBox2
+                    return new Bitmap(openFileDialog1.FileName);
                 }
                 catch (Exception ex)
                 {
-                    MessageBox.Show(ex.Message, "Erro ao abrir imagem...", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    MessageBox.Show($"Erro ao carregar imagem: {ex.Message}", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 }
             }
+            return null;
         }
 
         private void btnSum_Click(object sender, EventArgs e)
@@ -47,9 +65,8 @@ namespace ImageLoader
                 return;
             }
 
-            // Aplicar o aumento de brilho
-            img2 = AjustarBrilho(img2, sumValue);
-            pictureBox2.Image = img2;
+            img1 = AjustarBrilho(img1, sumValue);
+            pictureBoxResult.Image = img1;
             txtSum.Clear();
         }
 
@@ -61,11 +78,11 @@ namespace ImageLoader
                 return;
             }
 
-            // Aplicar a redução de brilho
-            img2 = AjustarBrilho(img2, -subValue);
-            pictureBox2.Image = img2;
+            img1 = AjustarBrilho(img1, -subValue);
+            pictureBoxResult.Image = img1;
             txtSubt.Clear();
-        }        
+        }
+
         private Bitmap AjustarBrilho(Bitmap img, int ajuste)
         {
             Bitmap tempImg = new Bitmap(img.Width, img.Height);
@@ -84,27 +101,121 @@ namespace ImageLoader
             }
             return tempImg;
         }
+               
         private void btnReset_Click_1(object sender, EventArgs e)
         {
-            img2 = new Bitmap(img1); // Restaura a imagem original
-            pictureBox2.Image = img2;
+            if (img1Original != null)
+            {
+                img1 = new Bitmap(img1Original);
+                pictureBoxResult.Image = img1;
+            }
+        }
+        private void btnMult_Click(object sender, EventArgs e)
+        {
+            if (!double.TryParse(txtMult.Text, out double multValue) || multValue < 0)
+            {
+                MessageBox.Show("Digite um valor válido maior ou igual a 0 para multiplicação.", "Entrada Inválida", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            img1 = AjustarMultiplicacao(new Bitmap(img1), multValue);
+            pictureBoxResult.Image = img1;
+            txtMult.Clear();
+        }
+        private Bitmap AjustarMultiplicacao(Bitmap img, double fator)
+        {
+            for (int i = 0; i < img.Width; i++)
+            {
+                for (int j = 0; j < img.Height; j++)
+                {
+                    Color pixel = img.GetPixel(i, j);
+                    int r = Math.Max(0, Math.Min(255, (int)(pixel.R * fator)));
+                    int g = Math.Max(0, Math.Min(255, (int)(pixel.G * fator)));
+                    int b = Math.Max(0, Math.Min(255, (int)(pixel.B * fator)));
+
+                    img.SetPixel(i, j, Color.FromArgb(pixel.A, r, g, b));
+                }
+            }
+            return img;
+        }
+
+
+        private void btndiv_Click(object sender, EventArgs e)
+        {
+            if (!double.TryParse(txtDiv.Text, out double divValue) || divValue <= 0)
+            {
+                MessageBox.Show("Digite um valor maior que 0 para divisão.", "Entrada Inválida", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            img1 = AjustarMultiplicacao(new Bitmap(img1), 1 / divValue);
+            pictureBoxResult.Image = img1;
+            txtDiv.Clear();
+        }
+
+        private void btnConvertGrayscale_Click(object sender, EventArgs e)
+        {
+            img1 = ConverterParaEscalaDeCinza(new Bitmap(img1));
+            pictureBoxResult.Image = img1;
+        }
+        private Bitmap ConverterParaEscalaDeCinza(Bitmap img)
+        {
+            for (int i = 0; i < img.Width; i++)
+            {
+                for (int j = 0; j < img.Height; j++)
+                {
+                    Color pixel = img.GetPixel(i, j);
+                    int gray = (int)(pixel.R * 0.3 + pixel.G * 0.59 + pixel.B * 0.11);
+                    img.SetPixel(i, j, Color.FromArgb(pixel.A, gray, gray, gray));
+                }
+            }
+            return img;
         }
 
 
 
-        private void Form1_Load(object sender, EventArgs e)
+        private void btnAdd_Click(object sender, EventArgs e)
         {
-
+            if (img1 == null || img2 == null)
+            {
+                MessageBox.Show("Carregue as duas imagens antes de somá-las.", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            img1 = MesclarImagens(img1, img2, true);
+            pictureBoxResult.Image = img1;
         }
 
-        private void label1_Click(object sender, EventArgs e)
+        private void btnReduce_Click(object sender, EventArgs e)
         {
-
+            if (img1 == null || img2 == null)
+            {
+                MessageBox.Show("Carregue as duas imagens antes de subtraí-las.", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            img1 = MesclarImagens(img1, img2, false);
+            pictureBoxResult.Image = img1;
         }
-
-        private void label4_Click(object sender, EventArgs e)
+        private Bitmap MesclarImagens(Bitmap imgA, Bitmap imgB, bool isAddition)
         {
+            Bitmap resultado = new Bitmap(imgA.Width, imgA.Height);
 
+            for (int i = 0; i < imgA.Width; i++)
+            {
+                for (int j = 0; j < imgA.Height; j++)
+                {
+                    Color pixelA = imgA.GetPixel(i, j);
+                    Color pixelB = imgB.GetPixel(i, j);
+
+                    int r = isAddition ? Math.Min(255, pixelA.R + pixelB.R) : Math.Max(0, pixelA.R - pixelB.R);
+                    int g = isAddition ? Math.Min(255, pixelA.G + pixelB.G) : Math.Max(0, pixelA.G - pixelB.G);
+                    int b = isAddition ? Math.Min(255, pixelA.B + pixelB.B) : Math.Max(0, pixelA.B - pixelB.B);
+
+                    resultado.SetPixel(i, j, Color.FromArgb(pixelA.A, r, g, b));
+                }
+            }
+            return resultado;
+        }
+        private void btnClean_Click(object sender, EventArgs e)
+        {
+            pictureBoxResult.Image = null;
         }
 
        
